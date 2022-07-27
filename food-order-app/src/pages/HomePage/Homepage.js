9b3ea5e564ea77a84eadecc3c647081904065cc2
Hung Trinh JH <hung.trinh@journeyh.io>
2022-07-27 21:15:43 +0700

    Update

--- food-order-app/src/pages/HomePage/Homepage.js
+++ food-order-app/src/pages/HomePage/Homepage.js
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 const Homepage = () => {
-  return (
-    <div>Homepage</div>
-  )
-}
+  const [meals, setMeals] = useState([]);
+  
+  const getMeals = async () => {
+    const data = await axios.get(
+      "https://625a91bf0ab4013f94a2d9a8.mockapi.io/meals"
+    );
+    console.log(data);
+    setMeals(data.data);
+    return data.data;
+  };
 
-export default Homepage
+  useEffect(() => {
+    getMeals();
+  }, []);
+
+  return <div>Homepage</div>;
+};
+
+export default Homepage;
