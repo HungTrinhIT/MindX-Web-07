3e8ccc61d74e7b2947e1cde65b1c04ec87a74be8
Hung Trinh JH <hung.trinh@journeyh.io>
2022-10-05 20:08:01 +0700

    Update

--- express-router-middleware/routes/User.js
+++ express-router-middleware/routes/User.js
@@ -1,15 +1,53 @@
 const express = require("express");
 const { db } = require("../config/db");
+const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
-// Get all
 router.get("/", async (req, res) => {
-  const users = await db.users.find({ age: { $gt: 19 } }).toArray();
+  const users = await db.users.find().toArray();
 
   res.json({
     data: users,
   });
+});
+
+router.post("/", async (req, res) => {
+  const { username, password, role, address } = req.body;
+
+  if (!username || !password || !role || !address) {
+    return res.status(400).json({
+      msg: "Missing required keys",
+    });
+  }
+
+  const user = await db.users.find({ username }).toArray();
+  const isAlreadyExistUser = user.length > 0;
+
+  if (isAlreadyExistUser) {
+    return res.json({
+      msg: "Username already exist, please try again",
+    });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, salt);
+
+  const newUser = {
+    username,
+    role,
+    address,
+    password: hashPassword,
+  };
+
+  try {
+    const response = await db.users.insertOne(newUser, {});
+    return res.status(201).json({
+      data: response,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 module.exports = router;
