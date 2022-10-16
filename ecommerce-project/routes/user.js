880427db00ab42f7ed656342bccccae1544c83c1
HungTrinhJH <hung.trinh@journeyh.io>
2022-10-16 11:48:23 +0700

    Update

--- ecommerce-project/routes/user.js
+++ ecommerce-project/routes/user.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const UserController = require("../controllers/user");
+const bcrypt = require("bcrypt");
 
 const router = express.Router();
+
+const hashPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  return hashedPassword;
+};
 
 router.post("/", async (req, res, next) => {
   const { fullname, password, email } = req.body;
 
-  // Validations
+  // Validation
   if (!fullname || !password || !email) {
     return res.json({
       msg: "Missing required keys",
@@ -15,10 +23,22 @@ router.post("/", async (req, res, next) => {
 
   try {
     const user = await UserController.findOne({ email });
-    console.log("user:", user);
-    
+    if (user) {
+      return res.status(400).json({
+        msg: "Email already exist, please try another one!",
+      });
+    }
+
+    const newUser = {
+      fullname,
+      password: await hashPassword(password),
+      email,
+    };
+
+    await UserController.create(newUser);
+
     return res.json({
-      msg: user,
+      msg: "User create successfully",
     });
   } catch (error) {
     console.error(error);
